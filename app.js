ea69d6d591738d326d7b28aa12498cf5efd84d74
Jonathan Rose <jrose@digium.com>
2015-01-19 10:59:47 -0600

    Only log errors to file, improve formatting.
    Does a little cheeky hack to the logger so that console.log is used
    to write error messages to standard IO while still preserving allowing
    the logger to write to files.

--- app.js
+++ app.js
@@ -13,14 +13,21 @@ var logger = require('voicemail-logging').create(
   require('./config.json'),
   'voicemail-admin');
 
-logger.info = function () {
-  var args = Array.prototype.slice.call(arguments, 0);
-  console.log.apply(this, args);
-};
+var errorFunc = logger.error;
 
+/* Increase level of streams that write to standard out for info/error
+ * loggers so that these streams are ignored. Bit of a hack. */
+for (var index in logger.streams) {
+  if (logger.streams[index].type === 'stream') {
+    logger.streams[index].level = 999;
+  }
+}
+
+/* Write error messages to the console as well as to the logger */
 logger.error = function () {
   var args = Array.prototype.slice.call(arguments, 0);
   console.log.apply(this, args);
+  errorFunc.apply(this, args);
 }
 
 var initializer = require('./lib/voicemail-admin-init.js');
